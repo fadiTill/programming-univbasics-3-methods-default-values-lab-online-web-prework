6ffdcd8142f791e3d154c96db044433478e03a28
fadimata tillman <fadi.tillman@gmail.com>
2019-12-26 18:16:21 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,12 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+def meal_choice(vegetables, meat )
+  meal choice =  true
+  meal choice ="Do you want #{vegetables= veg1,veg2} or #{meat = protein}
+  puts meal_choice
+end
+
+def nutritious_meal(protein, vegetables)
+nutitious_meal = "A plate of #{protein+vegetables} so that you can also see what you"ve ordered."
+puts nutitious_meal
+end
